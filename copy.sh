4301f292802fa56c7c61d7cb83aa63675b06939e
jiuhao47 <jiangjunyan22@mails.ucas.ac.cn>
2025-01-25 15:04:26 +0800

    ADD tex

--- copy.sh
+++ copy.sh
@@ -5,8 +5,10 @@ rm -rf ~/.config/dotfiles/zsh/
 rm -rf ~/.config/dotfiles/lvim/
 rm -rf ~/.config/dotfiles/neofetch/
 rm -rf ~/.config/dotfiles/tmux/
+rm -rf ~/.config/dotfiles/tex/
 
 cp -r ~/.config/zsh ./zsh/
 cp -r ~/.config/lvim ./lvim/
 cp -r ~/.config/neofetch ./neofetch/
 cp -r ~/.config/tmux ./tmux
+cp -r ~/.config/tex ./tex
