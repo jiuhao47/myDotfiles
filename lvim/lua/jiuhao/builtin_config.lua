5589cd9b3a6b669199b6920241e66a81a618114f
jiuhao47 <jiangjunyan22@mails.ucas.ac.cn>
2025-01-29 02:34:30 +0800

    ADD avante

--- lvim/lua/jiuhao/builtin_config.lua
+++ lvim/lua/jiuhao/builtin_config.lua
@@ -5,6 +5,9 @@ vim.o.shiftround = true
 vim.o.smartindent = true
 vim.o.exrc = true
 
+-- views can only be fully collapsed with the global statusline
+vim.opt.laststatus = 3
+
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.offsetEncoding = { "utf-16" }
 require("lspconfig").clangd.setup({ capabilities = capabilities })
