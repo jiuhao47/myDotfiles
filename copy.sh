086c9325c5672c081f9a3e81a289654e0a29220c
jiuhao47 <jiangjunyan22@mails.ucas.ac.cn>
2025-02-06 09:49:54 +0800

    ADD gdbplugins and modify proxy config

--- copy.sh
+++ copy.sh
@@ -6,9 +6,11 @@ rm -rf ~/.config/dotfiles/lvim/
 rm -rf ~/.config/dotfiles/neofetch/
 rm -rf ~/.config/dotfiles/tmux/
 rm -rf ~/.config/dotfiles/tex/
+rm -rf ~/.config/dotfiles/gdbplugins/
 
 cp -r ~/.config/zsh ./zsh/
 cp -r ~/.config/lvim ./lvim/
 cp -r ~/.config/neofetch ./neofetch/
 cp -r ~/.config/tmux ./tmux
 cp -r ~/.config/tex ./tex
+cp -r ~/.config/gdbplugins/ ./gdbplugins
