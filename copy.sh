2020e2cbfa8ec42f4361665ecdefffc294ebed79
jiuhao47 <jiangjunyan22@mails.ucas.ac.cn>
2025-11-20 14:50:41 +0800

    update bash

--- copy.sh
+++ copy.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-
-rm -rf ~/.config/dotfiles/zsh/
-rm -rf ~/.config/dotfiles/lvim/
+# rm -rf ~/.config/dotfiles/zsh/
+# rm -rf ~/.config/dotfiles/lvim/
+rm -rf ~/.config/dotfiles/bash/
 rm -rf ~/.config/dotfiles/neofetch/
 rm -rf ~/.config/dotfiles/tmux/
 rm -rf ~/.config/dotfiles/tex/
-rm -rf ~/.config/dotfiles/gdbplugins/
+# rm -rf ~/.config/dotfiles/gdbplugins/
 
-cp -r ~/.config/zsh ./zsh/
-cp -r ~/.config/lvim ./lvim/
+# cp -r ~/.config/zsh ./zsh/
+# cp -r ~/.config/lvim ./lvim/
+cp -r ~/.config/bash ./bash/
 cp -r ~/.config/neofetch ./neofetch/
 cp -r ~/.config/tmux ./tmux
 cp -r ~/.config/tex ./tex
-cp -r ~/.config/gdbplugins/ ./gdbplugins
+# cp -r ~/.config/gdbplugins/ ./gdbplugins
