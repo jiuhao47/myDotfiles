7fd148988bc4142f0f53957bb30c5567113cac19
jiuhao47 <jiangjunyan22@mails.ucas.ac.cn>
2025-11-20 15:07:31 +0800

    update

--- copy.sh
+++ copy.sh
@@ -2,16 +2,16 @@
 
 # rm -rf ~/.config/dotfiles/zsh/
 # rm -rf ~/.config/dotfiles/lvim/
-rm -rf ~/.config/dotfiles/bash/
-rm -rf ~/.config/dotfiles/neofetch/
-rm -rf ~/.config/dotfiles/tmux/
-rm -rf ~/.config/dotfiles/tex/
+rm -rf ~/.config/myDotfiles/bash
+rm -rf ~/.config/myDotfiles/neofetch
+rm -rf ~/.config/myDotfiles/tmux
+rm -rf ~/.config/myDotfiles/tex
 # rm -rf ~/.config/dotfiles/gdbplugins/
 
 # cp -r ~/.config/zsh ./zsh/
 # cp -r ~/.config/lvim ./lvim/
-cp -r ~/.config/bash ./bash/
-cp -r ~/.config/neofetch ./neofetch/
+cp -r ~/.config/bash ./bash
+cp -r ~/.config/neofetch ./neofetch
 cp -r ~/.config/tmux ./tmux
 cp -r ~/.config/tex ./tex
 # cp -r ~/.config/gdbplugins/ ./gdbplugins
